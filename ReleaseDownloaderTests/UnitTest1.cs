3d9b556b54b9071f8a2c074f099488bc6ec73900
Thomas Schissler <tschissler@agilemax.de>
2022-07-26 16:49:01 +0200

    Restructuring

---
+++ ReleaseDownloaderTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReleaseDownloaderTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
